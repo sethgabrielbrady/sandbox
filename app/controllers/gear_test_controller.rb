bc7e64f4b03f25be1d7908eda751ab9c91ca572c
Seth Brady <sethgabrielbrady@gmail.com>
2018-04-18 20:43:09 -0400

    added gear_test rout

---
+++ app/controllers/gear_test_controller.rb
@@ -0,0 +1,4 @@
+class GearTestController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/gear_test', to:'gear_test#index'
   get 'home/index'
 
   root to: 'home#index'
